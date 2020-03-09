fa308fea3767ee5ea8f3bd23e06b80811dc80795
master <master@rivne.itstep.org>
2020-03-09 18:50:42 +0200

    Init empty project

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+const App = () => {
+  return <h1>Contact list</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
